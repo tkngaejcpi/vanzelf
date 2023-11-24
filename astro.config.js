29350042670be70578d11c39298f753be8a88702
Riley Chang <rileycki3333@gmail.com>
2023-11-24 19:55:35 +0800

    remove specific article redirects

--- astro.config.js
+++ astro.config.js
@@ -8,10 +8,6 @@ export default defineConfig({
   site: "https://blog.rileycki.com",
   redirects: {
     "/writing": "/articles",
-    "/writing/reboot-to-a-brand-new-system-with-btrfs":
-      "/articles/Reboot to a Brand New System with Btrfs",
-    "/writing/boot-your-system-to-previous-state-with-btrfs-mandarin":
-      "/articles/利用 btrfs 開機還原系統",
   },
 
   integrations: [tailwind(), sitemap()],
